51e3b41103d637aa5090a5d200f70933dc193459
MADFutcher <mikalob247@gmail.com>
2021-02-10 19:38:50 +0100

    Adjusted colours and added Login and Signup form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './colour.css'
+import './App.css'
+import React, { Component } from 'react'
+import Login from './components/login/Login'
+import Signup from './components/signup/Signup'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, Route, Switch } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      isLoggedin: false
+    }
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <div className='App-header'>
+          <h1 className='text-indigo display-1'>MELLIFERA &#178;</h1>
+          <div>
+            <Login />
+            <Link to="/signup">
+                Signup
+            </Link>
+            <Switch>
+                <Route path='/signup'>
+                  <Signup />
+                </Route>
+            </Switch>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+
---
+++ src/components/signup/Signup.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+
+
+export default class Signup extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            firstname:'',
+            surname: '',
+            username:'',
+            password:'',
+        }
+    }
+
+    handleOnChange = (e) =>{
+        const name = e.target.name
+        const val = e.target.value
+
+        this.setState({
+            [name]:val
+        })
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-12'>
+                        <Form>
+                            <Form.Row>
+                                <Col>
+                                    <Form.Group controlId="formBasicFirstname">
+                                        <Form.Label>First name</Form.Label>
+                                        <Form.Control type="text" name="firstname" placeholder="Enter Firstname"  onChange={this.handleOnChange}/>
+                                    </Form.Group>
+                                </Col>
+                                <Col>
+                                    <Form.Group controlId="formBasicSurname">
+                                        <Form.Label>Surname</Form.Label>
+                                        <Form.Control type="text" name="surname" placeholder="Enter Surname"  onChange={this.handleOnChange}/>
+                                    </Form.Group>
+                                </Col>
+                            </Form.Row>
+                            <Form.Row>
+                                <Col>
+                                    <Form.Group controlId="formBasicUsername">
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control type="text" name="username" placeholder="Enter Username"  onChange={this.handleOnChange}/>
+                                    </Form.Group>
+                                </Col>
+                                <Col>
+                                    <Form.Group controlId="formBasicPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" name="password" placeholder="Password" onChange={this.handleOnChange}/>
+                                    </Form.Group>
+                                </Col>
+                            </Form.Row>
+                            <Button variant='secondary' type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
